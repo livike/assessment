6fb9068b90c62a91986a8b15e05ba8daccc49c7d
Dobai Lívia <livia.dobai@gmail.com>
2020-01-24 21:17:26 +0100

    Adding only number control to the input field

--- js-numerals/index.js
+++ js-numerals/index.js
@@ -4,6 +4,12 @@ const initApp = () => {
   newButton.addEventListener('click', convertNumber);
 };
 
+const isNumberKey = (evt) => {
+  let charCode = (evt.which) ? evt.which : evt.keyCode
+  if (charCode > 31 && (charCode < 48 || charCode > 57))
+      return false;
+  return true;
+}
 
 const createElement = (type, original, text, className) => {
   // Creates a new HTML element and returns it
